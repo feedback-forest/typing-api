9f34abed07dc67ba61a67e20763f1fc18fbd30b5
YongBum <dt10002@naver.com>
2025-03-11 11:21:05 +0900

    :white_check_mark: Test: 최대 20개의 문장 랜덤 조회 기능 테스트
    - Entity id를 가져오기 위해서 Phrase 어노테이션 Getter 추가
    - Junit 활용하여 문장 랜덤 조회 기능 테스트 구현
    issue #3

---
+++ src/test/java/dasi/typing/domain/phrase/PhraseRepositoryTest.java
@@ -0,0 +1,64 @@
+package dasi.typing.domain.phrase;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class PhraseRepositoryTest {
+
+  @Autowired
+  private PhraseRepository phraseRepository;
+
+  @AfterEach
+  void tearDown() {
+    phraseRepository.deleteAllInBatch();
+  }
+
+  @Test
+  @DisplayName("랜덤으로 20개의 문장들을 조회할 수 있다.")
+  public void findRandom20Phrase() {
+    // given
+    List<Phrase> phrases = new ArrayList<>();
+    for (int i = 1; i <= 25; i++) {
+      phrases.add(createPhrase(String.valueOf(i), "문장 " + i, "작가 " + i, Lang.KO, LangType.QUOTE));
+    }
+    phraseRepository.saveAll(phrases);
+
+    // when
+    List<Long> allIds = phrases.stream().map(Phrase::getId).toList();
+
+    List<Long> firstResult = phraseRepository.findRandom20Phrases()
+        .stream().map(Phrase::getId).toList();
+
+    List<Long> secondResult = phraseRepository.findRandom20Phrases()
+        .stream().map(Phrase::getId).toList();
+
+    // then
+    assertThat(allIds)
+        .containsAll(firstResult)
+        .containsAll(secondResult);
+    assertThat(firstResult).hasSize(20).hasSameSizeAs(secondResult);
+    assertThat(firstResult).isNotEqualTo(secondResult);
+  }
+
+
+  private Phrase createPhrase(String sentence, String title, String author, Lang lang,
+      LangType types) {
+    return Phrase.builder()
+        .sentence(sentence)
+        .title(title)
+        .author(author)
+        .lang(lang)
+        .types(types)
+        .build();
+  }
+}
