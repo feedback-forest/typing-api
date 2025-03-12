59006de8df866eaf1493a82af9e84df9324ab805
YongBum <dt10002@naver.com>
2025-03-13 02:05:33 +0900

    :sparkles: Feat: GlobalExceptionHandler 구현
    - RestController 전역으로 관리하기 위한 GlobalExceptionHandler 클래스를 구현합니다.
    - 예외 처리를 함에 있어서 예외 코드들을 커스텀하기 위해 enum 클래스를 활용해 구현합니다.
    - API 호출 성공 Response, 예외 발생 Response 응답 객체인 ApiResponse 클래스를 구현합니다.
    issue #5

---
+++ src/test/java/dasi/typing/exception/ApiResponseTest.java
@@ -0,0 +1,53 @@
+package dasi.typing.exception;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class ApiResponseTest {
+
+  @Test
+  @DisplayName("success 응답이 정상적으로 생성된다.")
+  public void successApiResponse() {
+    // given
+    String data = "응답 성공";
+
+    // when
+    ApiResponse<?> response = ApiResponse.success(data);
+
+    // then
+    assertThat(response)
+        .extracting("code", "message", "data")
+        .containsExactly(
+            Code.OK.getCode(),
+            Code.OK.getMessage(),
+            data
+        );
+  }
+
+  @Test
+  @DisplayName("error 응답이 정상적으로 생성된다.")
+  public void errorApiResponse() {
+    // given
+    Code errorCode = Code.INVALID_CHARACTER_NICKNAME;
+
+    // when
+    ApiResponse<Void> response = ApiResponse.error(errorCode);
+
+    // then
+    assertThat(response)
+        .extracting("code", "message", "data")
+        .containsExactly(
+            Code.INVALID_CHARACTER_NICKNAME.getCode(),
+            Code.INVALID_CHARACTER_NICKNAME.getMessage(),
+            null
+        );
+  }
+
+}
