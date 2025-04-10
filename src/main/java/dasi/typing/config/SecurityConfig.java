09c87b6f76625474fd48f1da2101d7210caca7f5
YongBum <dt10002@naver.com>
2025-04-10 15:53:20 +0900

    :sparkles: Feat: OAuth2 로그인 Security 설정 추가
    - OAuth2 Client 기능을 활성화합니다.
    - OAuth2 로그인 과정 중, 사용자의 정보를 가져오기 위한 작업을 customOAuth2UserService 파일에서 수행하도록 연결해주었습니다.
    - OAuth2 로그인 성공 시, 어떤 동작을 해야할 지 지정해주기 위해서 successHandler 기능을 설정해주었습니다.
    - 마찬가지로, 성공했을 때의 동작은 oAuth2AuthenticationSuccessHandler 파일에서 수행하도록 연결해주었습니다.
    
    issue #13

--- src/main/java/dasi/typing/config/SecurityConfig.java
+++ src/main/java/dasi/typing/config/SecurityConfig.java
@@ -1,7 +1,11 @@
 package dasi.typing.config;
 
+import dasi.typing.api.service.oauth.CustomOAuth2UserService;
+import dasi.typing.handler.OAuth2AuthenticationSuccessHandler;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
@@ -13,7 +17,11 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+  private final CustomOAuth2UserService customOAuth2UserService;
+  private final OAuth2AuthenticationSuccessHandler oAuth2AuthenticationSuccessHandler;
 
   @Bean
   public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -25,6 +33,15 @@ public class SecurityConfig {
         .logout(LogoutConfigurer::disable)
         .authorizeHttpRequests(auth -> auth
             .anyRequest().permitAll()
+        );
+
+    http
+        .oauth2Client(Customizer.withDefaults())
+        .oauth2Login(oauth2 -> oauth2
+            .successHandler(oAuth2AuthenticationSuccessHandler)
+            .userInfoEndpoint(userInfo -> userInfo
+                .oidcUserService(customOAuth2UserService)
+            )
         );
 
     http.sessionManagement(
