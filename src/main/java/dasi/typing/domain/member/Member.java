61ba9e32597a266475487e8c5f8efd3a15d0a1b7
YongBum <dt10002@naver.com>
2025-04-22 22:10:30 +0900

    :sparkles: Feat: 약관 동의 항목 Entity 구현
    - 유저 회원가입 시, 동의한 약관 내용을 저장하기 위해서 새로운 Consent, MemberConsent Entity를 구현했습니다.
    - 하나의 유저는 여러 개의 약관에 동의할 수 있고, 하나의 약관은 여러 명의 유저가 동의합니다.
    - N:M 관계이므로 이를 MemberConsent 라는 중간 Table을 만들어 1:N, M:1 관계로 풀어서 구현했습니다.
    - Consent 데이터는 정적이고, MemberConsent 데이터는 Member 정보에 의존합니다.
    - Member Entity 데이터의 저장으로 MemberConsent Entity 측 데이터도 함께 저장하기 위해서 단방향 연관관계 매핑을 활용했습니다.
    - ManyToOne 어노테이션의 기본값이 EAGER 이기 때문에, 필요한 정보만 가져오기 위해서 LAZY로 설정하였습니다.
    
    issue #16

--- src/main/java/dasi/typing/domain/member/Member.java
+++ src/main/java/dasi/typing/domain/member/Member.java
@@ -1,10 +1,17 @@
 package dasi.typing.domain.member;
 
 import dasi.typing.domain.BaseEntity;
+import dasi.typing.domain.memberConsent.MemberConsent;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
+import java.util.ArrayList;
+import java.util.List;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
@@ -19,13 +26,23 @@ public class Member extends BaseEntity {
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
 
+  @Column(unique = true)
   private String kakaoId;
 
+  @Column(unique = true)
   private String nickname;
+
+  @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
+  @JoinColumn(name = "member_id")
+  private List<MemberConsent> agreements = new ArrayList<>();
 
   @Builder
   private Member(String kakaoId, String nickname) {
     this.kakaoId = kakaoId;
     this.nickname = nickname;
+  }
+
+  public void addConsent(MemberConsent memberConsent) {
+    agreements.add(memberConsent);
   }
 }
