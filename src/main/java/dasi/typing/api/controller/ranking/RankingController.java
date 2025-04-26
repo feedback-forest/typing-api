556fc41b4f5c80d94b86978862d72968ae585cdd
YongBum <dt10002@naver.com>
2025-04-27 00:06:53 +0900

    :sparkles: Feat: 타자 결과에 대한 종합 점수를 토대로 순위 조회 기능 구현
    - 기획 내용을 반영하여 score -> maxCpm -> acc -> createdDate -> id 순서대로 순위를 정합니다.
    - 해당 순위를 Repository 쿼리 메소드를 통해서 데이터를 불러옵니다.
    - 테스트를 위해서 사용한 RankingResponse 필드 변수는 JsonIgnore 어노테이션을 통해서 응답에서 제외합니다.
    
    issue #19

--- src/main/java/dasi/typing/api/controller/ranking/RankingController.java
+++ src/main/java/dasi/typing/api/controller/ranking/RankingController.java
@@ -4,30 +4,29 @@ import dasi.typing.api.controller.ranking.response.RankingResponse;
 import dasi.typing.api.service.ranking.RankingService;
 import dasi.typing.exception.ApiResponse;
 import java.util.List;
+import java.util.Map;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@PreAuthorize("hasAnyRole('USER', 'GUEST')")
 public class RankingController {
 
   private final RankingService rankingService;
 
   @GetMapping("/api/v1/rankings/realtime")
-  public ApiResponse<List<RankingResponse>> getRealTimeRanking() {
-
+  public ApiResponse<Map<String, List<RankingResponse>>> getRealTimeRanking() {
     List<RankingResponse> responses = rankingService.getRealTimeRanking();
-
-    return ApiResponse.success(responses);
+    return ApiResponse.success("rankings", responses);
   }
 
   @GetMapping("/api/v1/rankings/monthly")
-  public ApiResponse<List<RankingResponse>> getMonthlyRanking() {
-
+  public ApiResponse<Map<String, List<RankingResponse>>> getMonthlyRanking() {
     List<RankingResponse> responses = rankingService.getMonthlyRanking();
-
-    return ApiResponse.success(responses);
+    return ApiResponse.success("rankings", responses);
   }
 
 }
--- src/main/java/dasi/typing/api/service/typing/request/TypingCreateServiceRequest.java
+++ src/main/java/dasi/typing/api/service/typing/request/TypingCreateServiceRequest.java
@@ -3,12 +3,13 @@ package dasi.typing.api.service.typing.request;
 import dasi.typing.domain.member.Member;
 import dasi.typing.domain.phrase.Phrase;
 import dasi.typing.domain.typing.Typing;
+import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
-@NoArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class TypingCreateServiceRequest {
 
   private Long phraseId;
@@ -19,12 +20,16 @@ public class TypingCreateServiceRequest {
 
   private Integer wpm;
 
+  private Integer maxCpm;
+
   @Builder
-  private TypingCreateServiceRequest(Long phraseId, Integer cpm, Integer acc, Integer wpm) {
+  private TypingCreateServiceRequest(Long phraseId, Integer cpm, Integer acc, Integer wpm,
+      Integer maxCpm) {
     this.phraseId = phraseId;
     this.cpm = cpm;
     this.acc = acc;
     this.wpm = wpm;
+    this.maxCpm = maxCpm;
   }
 
   public Typing toEntity(Phrase phrase, Member member) {
@@ -33,6 +38,7 @@ public class TypingCreateServiceRequest {
         .phrase(phrase)
         .cpm(cpm)
         .acc(acc)
-        .wpm(wpm).build();
+        .wpm(wpm)
+        .maxCpm(maxCpm).build();
   }
 }
