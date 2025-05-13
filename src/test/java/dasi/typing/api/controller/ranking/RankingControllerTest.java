c15a14be1bd2813b5dabd5b3579ef257c9c4ba63
YongBum <dt10002@naver.com>
2025-05-13 17:51:51 +0900

    :white_check_mark: Test: RankingController 단위 테스트
    - RankingController 단위 테스트를 진행했습니다.
    - 기존에 복잡했던 테스트 코드를 BDD 스타일로 명확하게 구분하여 코드를 재구성했습니다.
    - Security 인증 객체를 WithMockUser 어노테이션을 통해서 가짜 객체를 만들어 원활한 테스트가 되도록 구현했습니다.
    
    issue #36

--- src/test/java/dasi/typing/api/controller/ranking/RankingControllerTest.java
+++ src/test/java/dasi/typing/api/controller/ranking/RankingControllerTest.java
@@ -1,56 +1,83 @@
 package dasi.typing.api.controller.ranking;
 
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.verify;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import dasi.typing.ControllerTestSupport;
+import dasi.typing.api.controller.ranking.response.RankingResponse;
+import dasi.typing.exception.ApiResponse;
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Map;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.annotation.DirtiesContext;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.jdbc.Sql;
-import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.security.test.context.support.WithMockUser;
 
-@SpringBootTest
-@AutoConfigureMockMvc
-@ActiveProfiles("test")
-@Sql(scripts = "/ranking.sql")
-@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
-class RankingControllerTest {
-
-  @Autowired
-  private MockMvc mockMvc;
+@WithMockUser
+class RankingControllerTest extends ControllerTestSupport {
 
   @Test
-  @DisplayName("실시간 랭킹 데이터를 조회하여 응답을 정상적으로 반환한다.")
-  void sendRealTimeRankingToClient() throws Exception {
-    // when & then
+  @DisplayName("실시간 랭킹 데이터를 조회하여 정상적으로 응답을 반환한다.")
+  void sendRealTimeRankingToClientTest() throws Exception {
+
+    // given
+    List<RankingResponse> responses = createRankingResponse();
+    given(rankingService.getRealTimeRanking())
+        .willReturn(responses);
+
+    // when
+    ApiResponse<Map<String, List<RankingResponse>>> expected
+        = ApiResponse.success("rankings", responses);
+    String expectedJson = objectMapper.writeValueAsString(expected);
+
+    // then
     mockMvc.perform(
             get("/api/v1/rankings/realtime")
         )
         .andDo(print())
         .andExpect(status().isOk())
-        .andExpect(jsonPath("$.code").value("0"))
-        .andExpect(jsonPath("$.message").value("success"))
-        .andExpect(jsonPath("$.data").isMap());
+        .andExpect(content().json(expectedJson));
+
+    verify(rankingService).getRealTimeRanking();
   }
 
   @Test
-  @DisplayName("현재 날짜에 해당하는 연월 랭킹 데이터를 조회하여 응답을 정상적으로 반환한다.")
-  void sendMonthlyRankingToClient() throws Exception {
-    // when & then
+  @DisplayName("현재 날짜에 해당하는 연월 랭킹 데이터를 조회하여 정상적으로 응답을 반환한다.")
+  void sendMonthlyRankingToClientTest() throws Exception {
+
+    // given
+    List<RankingResponse> responses = createRankingResponse();
+    given(rankingService.getMonthlyRanking())
+        .willReturn(responses);
+
+    // when
+    ApiResponse<Map<String, List<RankingResponse>>> expected
+        = ApiResponse.success("rankings", responses);
+    String expectedJson = objectMapper.writeValueAsString(expected);
+    System.out.println(expectedJson);
+
+    // then
     mockMvc.perform(
             get("/api/v1/rankings/monthly")
         )
         .andDo(print())
         .andExpect(status().isOk())
-        .andExpect(jsonPath("$.code").value("0"))
-        .andExpect(jsonPath("$.message").value("success"))
-        .andExpect(jsonPath("$.data").isMap());
+        .andExpect(content().json(expectedJson));
+
+    verify(rankingService).getMonthlyRanking();
   }
 
+  private static List<RankingResponse> createRankingResponse() {
+    return List.of(RankingResponse.builder()
+        .memberId(1L)
+        .nickname("dragon")
+        .score(180)
+        .createdDate(Timestamp.valueOf(LocalDateTime.now()))
+        .ranking(1L).build());
+  }
 }
