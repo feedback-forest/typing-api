52e24ccac9e5680ac7863489898b2dfb4a0f7a1d
Yongbum Kim <dt10002@naver.com>
2026-04-26 15:12:11 +0900

    feat: Redis 장애에 대비하기 위한 설정 구현
    - Redis 장애를 감지하기 위해서 응답 시간을 300ms로 설정했습니다.
    - 단일 Redis 노드와 연결하기 위해서 Standalone 설정을 사용했습니다.
    - 장애로 인해 내려갔다가 다시 정상 구동했을 때, 빠른 연결을 위해 autoReconnect 설정을 추가했습니다.

--- src/main/java/dasi/typing/config/RedisConfig.java
+++ src/main/java/dasi/typing/config/RedisConfig.java
@@ -1,9 +1,14 @@
 package dasi.typing.config;
 
+import io.lettuce.core.ClientOptions;
+import io.lettuce.core.SocketOptions;
+import java.time.Duration;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
@@ -17,9 +22,32 @@ public class RedisConfig {
   @Value("${spring.data.redis.port}")
   private int redisPort;
 
+  @Value("${spring.data.redis.connect-timeout-millis:200}")
+  private long redisConnectTimeoutMillis;
+
+  @Value("${spring.data.redis.command-timeout-millis:300}")
+  private long redisCommandTimeoutMillis;
+
   @Bean
   public RedisConnectionFactory redisConnectionFactory() {
-    return new LettuceConnectionFactory(redisHost, redisPort);
+    RedisStandaloneConfiguration redisConfiguration = new RedisStandaloneConfiguration(redisHost, redisPort);
+
+    SocketOptions socketOptions = SocketOptions.builder()
+        .connectTimeout(Duration.ofMillis(redisConnectTimeoutMillis))
+        .build();
+
+    ClientOptions clientOptions = ClientOptions.builder()
+        .socketOptions(socketOptions)
+        .autoReconnect(true)
+        .build();
+
+    LettuceClientConfiguration clientConfiguration = LettuceClientConfiguration.builder()
+        .commandTimeout(Duration.ofMillis(redisCommandTimeoutMillis))
+        .shutdownTimeout(Duration.ZERO)
+        .clientOptions(clientOptions)
+        .build();
+
+    return new LettuceConnectionFactory(redisConfiguration, clientConfiguration);
   }
 
   @Bean
